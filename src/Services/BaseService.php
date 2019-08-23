09ffb880b4b919f2ec5ba4a8dc3812b4c319c916
Daniel Jones <danny@sotamedia.co.uk>
2019-08-23 10:33:48 +0100

    BaseService > updates

--- src/Services/BaseService.php
+++ src/Services/BaseService.php
@@ -3,6 +3,8 @@
 namespace Dannerz\Api\Services;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class BaseService
 {
@@ -15,7 +17,7 @@ class BaseService
 
     public function create(array $data)
     {
-        return $this->model->create($data)->fresh();
+        return $this->save(new $this->model, $data)->fresh();
     }
 
     public function delete($id)
@@ -25,18 +27,69 @@ class BaseService
 
     public function update($id, array $data)
     {
-        $model = $this->model->findOrFail($id);
+        return $this->save($this->model->findOrFail($id), $data)->fresh();
+    }
 
-        $model->update($data);
+    protected function save(Model $model, array $data): Model
+    {
+        $hasManyRelationKeys = $this->getRelationKeys($model, $data, HasMany::class);
+        $belongsToManyRelationKeys = $this->getRelationKeys($model, $data, BelongsToMany::class);
+
+        $attributes = array_except($data, array_merge($belongsToManyRelationKeys, $hasManyRelationKeys));
+        $hasManyRelationsToSync = array_only($data, $hasManyRelationKeys);
+        $belongsToManyRelationsToSync = array_only($data, $belongsToManyRelationKeys);
+
+        $model->fill($attributes);
+
+        $model->save();
+
+        foreach ($hasManyRelationsToSync as $hasManyRelation => $rows) {
+            if (! is_array($rows)) continue;
+            $this->syncHasManyRelationModels($model, camel_case($hasManyRelation), collect($rows));
+        }
+
+        foreach ($belongsToManyRelationsToSync as $belongsToManyRelation => $ids) {
+            if (! is_array($ids)) continue;
+            $model->{camel_case($belongsToManyRelation)}()->sync($ids);
+        }
 
         return $model->fresh();
     }
 
-    // TODO: Implement basic child & pivot handling again.
+    protected function getRelationKeys($model, $data, $relationClass)
+    {
+        $relationKeys = [];
 
-    // TODO: Implement basic delete guard? Where guards against any children?
+        foreach (array_keys($data) as $key) {
+            $method = camel_case($key);
+            if (method_exists($model, $method)) {
+                if (get_class($model->$method()) == $relationClass) {
+                    $relationKeys[] = $key;
+                }
+            }
+        }
 
-    // TODO: Make commands.
+        return $relationKeys;
+    }
 
-    // TODO: Spatie media?
+    protected function syncHasManyRelationModels($model, $hasManyRelation, $rows)
+    {
+        $hasManyModel = $model->$hasManyRelation()->getRelated();
+
+        // Deleted.
+        $rowIds = $rows->pluck($hasManyModel->getKeyName())->filter()->all();
+        $model->$hasManyRelation->each(function ($hasManyModel) use ($rowIds) {
+            if (! in_array($hasManyModel->getKey(), $rowIds)) $hasManyModel->delete();
+        });
+
+        // Created.
+        $rows->where($hasManyModel->getKeyName(), null)->each(function ($attributes) use ($model, $hasManyRelation, $hasManyModel) {
+            $model->$hasManyRelation()->create($attributes);
+        });
+
+        // Updated.
+        $rows->where($hasManyModel->getKeyName(), '!=', null)->each(function ($attributes) use ($hasManyModel) {
+            $hasManyModel->findOrFail($attributes[$hasManyModel->getKeyName()])->update($attributes);
+        });
+    }
 }
